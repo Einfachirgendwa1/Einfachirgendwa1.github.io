8c3dfc1cd7863379674639bc6e31547cc3587128
Einfach_Irgendwa1 <faris.karge@berlin>
2024-10-27 20:51:55 +0100

    refactor some code and automatically recalculate on input

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 import init, { lagrange } from "../pkg/pages.js";
 
+const x = document.getElementById("x-value");
+const points = document.getElementById("points");
+const output = document.getElementById("output");
+
+async function recalculate() {
+  output.textContent = lagrange(x.value, points.value);
+}
+
 async function run() {
   await init();
 
-  const form = document.getElementById("lagrange-form");
-  const output = document.getElementById("output");
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const x = document.getElementById("x-value").value;
-    const pointsInput = document.getElementById("points").value;
-
-    const result = lagrange(x, pointsInput);
-    output.textContent = result;
-  });
+  x.addEventListener("input", recalculate);
+  points.addEventListener("input", recalculate);
 }
 
 run();
