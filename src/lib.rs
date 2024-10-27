0566bdc6be46469db18e2881924e622f6b89d87d
Einfach_Irgendwa1 <faris.karge@berlin>
2024-10-27 18:55:38 +0100

    no way this works the index.html is fully written by chatgpt and i have not tested this

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub struct Point {
+    x: f64,
+    y: f64,
+}
+
+pub fn lagrange(x: f64, points: Vec<Point>) -> f64 {
+    points
+        .iter()
+        .map(|point| {
+            point.y
+                * points
+                    .iter()
+                    .filter(|inner_point| inner_point.x != point.x)
+                    .map(|inner_point| (x - inner_point.x) / (point.x - inner_point.x))
+                    .product::<f64>()
+        })
+        .sum()
+}
